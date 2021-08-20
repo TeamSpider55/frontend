6253657816f8f005350247ea42d157c4c60b9cd3
jonjau <jonjau3q@gmail.com>
2021-08-20 16:19:24 +1000

    add material UI template

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
+import MiniDrawer from './components/Sidebar';
 import PageNotFound from './pages/PageNotFound';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" />
+        <Route exact path="/">
+          <MiniDrawer />
+        </Route>
         <Route>
           {/* matches any other route: page for 404 error */}
           <PageNotFound />
