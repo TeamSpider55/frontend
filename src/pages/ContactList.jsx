689b26eb0cdf783b9c0c822db5fa50c33e5aca64
jonjau <jonjau3q@gmail.com>
2021-09-19 16:50:08 +1000

    fixed select all, and alignment issue with header

--- src/pages/ContactList.jsx
+++ src/pages/ContactList.jsx
@@ -30,9 +30,9 @@ const useStyles = makeStyles((theme) => ({
 
 const TABLE_HEAD = [
   { id: 'name', label: 'Name', alignRight: false },
-  // { id: 'company', label: 'Company', alignRight: false },
-  // { id: 'role', label: 'Role', alignRight: false },
-  // { id: '' },
+  { id: 'organisation', label: 'Organisation', alignRight: false },
+  { id: 'role', label: 'Role', alignRight: false },
+  { id: 'dateAdded', label: 'Date Added', alignRight: false },
 ];
 
 const ContactList = () => {
@@ -59,20 +59,21 @@ const ContactList = () => {
     setOrderBy(property);
   };
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelecteds = contacts.map((n) => n.name);
+  const handleSelectAllClick = () => {
+    if (selected.length === 0) {
+      // select all
+      const newSelecteds = contacts.map((n) => n.contactId);
       setSelected(newSelecteds);
       return;
     }
     setSelected([]);
   };
 
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
+  const handleClick = (event, contactId) => {
+    const selectedIndex = selected.indexOf(contactId);
     let newSelected = [];
     if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
+      newSelected = newSelected.concat(selected, contactId);
     } else if (selectedIndex === 0) {
       newSelected = newSelected.concat(selected.slice(1));
     } else if (selectedIndex === selected.length - 1) {
@@ -97,6 +98,10 @@ const ContactList = () => {
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
+  };
+
+  const goToContact = (id) => {
+    history.push(`/contacts/${id}`);
   };
 
   const emptyRows = page > 0
@@ -158,7 +163,7 @@ const ContactList = () => {
                             familyName,
                           } = row;
                         const name = `${givenName} ${familyName}`;
-                        const isItemSelected = selected.indexOf(name) !== -1;
+                        const isItemSelected = selected.indexOf(contactId) !== -1;
 
                         return (
                           <TableRow
@@ -172,30 +177,35 @@ const ContactList = () => {
                             <TableCell padding="checkbox">
                               <Checkbox
                                 checked={isItemSelected}
-                                onChange={(event) => handleClick(event, name)}
+                                onChange={(event) => handleClick(event, contactId)}
                               />
                             </TableCell>
-                            <Box onClick={() => history.push('/contacts/1')}>
-                              <TableCell
-                                component="th"
-                                scope="row"
-                              >
-                                <Box display="flex" alignItems="center">
-                                  <Avatar alt={name} src="#FIXME: URL" />
-                                  <Box
-                                    component={Typography}
-                                    variant="subtitle2"
-                                    noWrap
-                                    paddingLeft={2}
-                                  >
-                                    {name}
-                                  </Box>
+                            <TableCell
+                              component="th"
+                              scope="row"
+                              onClick={() => goToContact(1)}
+                            >
+                              <Box display="flex" alignItems="center">
+                                <Avatar alt={name} src="#FIXME: URL" />
+                                <Box
+                                  component={Typography}
+                                  variant="subtitle2"
+                                  noWrap
+                                  paddingLeft={2}
+                                >
+                                  {name}
                                 </Box>
-                              </TableCell>
-                              <TableCell align="left">Apple</TableCell>
-                              <TableCell align="left">Software Engineer</TableCell>
-                              <TableCell align="left">17 August 2021</TableCell>
-                            </Box>
+                              </Box>
+                            </TableCell>
+                            <TableCell onClick={() => goToContact(1)}>
+                              Apple
+                            </TableCell>
+                            <TableCell onClick={() => goToContact(1)}>
+                              Software Engineer
+                            </TableCell>
+                            <TableCell onClick={() => goToContact(1)}>
+                              17 August 2021
+                            </TableCell>
                             <TableCell align="right">
                               <MoreMenu />
                             </TableCell>
