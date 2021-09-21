f9addedd51a1010f640290bd0ce3f8c440cc0be0
andylu0129 <meichuanl@student.unimelb.edu.au>
2021-09-21 13:40:36 +1000

    added spider icon to login page and hyperlink that navigate to register page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,26 @@
 import React from 'react';
 import {
-  Button, Box, TextField, makeStyles,
+  Button, Box, TextField, makeStyles, Link,
 } from '@material-ui/core';
+import { Link as RouterLink } from 'react-router-dom';
 import { useTheme } from '@material-ui/core/styles';
 import ExitToAppIcon from '@material-ui/icons/ExitToApp';
 import Logo from '../components/Logo';
+import SpiderIcon from '../assets/spider1.png';
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    minHeight: '100vh',
+    backgroundColor: theme.palette.background.neutral,
+  },
+  signInLink: {
+    textAlign: 'right',
+    padding: theme.spacing(2),
+  },
+  formContainer: {
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
-    minHeight: '100vh',
-    backgroundColor: theme.palette.background.neutral,
   },
   form: {
     display: 'flex',
@@ -36,18 +44,45 @@ const useStyles = makeStyles((theme) => ({
 const Login = () => {
   const theme = useTheme();
   const classes = useStyles(theme);
+
   return (
     <Box className={classes.root}>
-      <Box className={classes.form}>
-        <Box textAlign="center" paddingY={theme.spacing(1)}>
-          <Logo />
+      <Box className={classes.signInLink}>
+        Don&apos;t have an account?
+        {' '}
+        <Link component={RouterLink} to="/register">
+          <Box display="inline" fontWeight={theme.typography.fontWeightBold}>
+            Join now
+          </Box>
+        </Link>
+      </Box>
+      <Box className={classes.formContainer}>
+        <Box className={classes.form}>
+          <Box textAlign="center" paddingBottom={theme.spacing(1)}>
+            <img src={SpiderIcon} alt="spider" width="100" height="100" />
+            <Logo />
+          </Box>
+          <TextField
+            className={classes.formRow}
+            label="Email Address"
+            variant="outlined"
+            type="email"
+          />
+          <TextField
+            className={classes.formRow}
+            label="Password"
+            variant="outlined"
+            type="password"
+          />
+          <Button
+            className={classes.formButton}
+            variant="contained"
+            color="primary"
+          >
+            <ExitToAppIcon />
+            Login
+          </Button>
         </Box>
-        <TextField className={classes.formRow} label="Email Address" variant="outlined" />
-        <TextField className={classes.formRow} label="Password" variant="outlined" />
-        <Button className={classes.formButton} variant="contained" color="primary">
-          <ExitToAppIcon />
-          Login
-        </Button>
       </Box>
     </Box>
   );
