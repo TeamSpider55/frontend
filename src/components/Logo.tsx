d3022b231ca1b772e28a3f1cd38b2d9585451b36
andylu0129 <meichuanl@student.unimelb.edu.au>
2021-09-23 20:46:27 +1000

    added box shadow (outline) when editing contact detail, adjusted contact name to input that it can be changed, disabled highlighting on the OneThread header logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -10,14 +10,32 @@ interface LogoHighlightProps {
 const LogoHighlight = styled('span')(({ theme }: LogoHighlightProps) => ({
   color: theme.palette.primary.light,
   fontWeight: theme.typography.fontWeightBold,
+  '-moz-user-select': 'none',
+  '-webkit-user-select': 'none',
+  '-ms-user-select': 'none',
+  'user-select': 'none',
+  '-o-user-select': 'none',
+}));
+
+const LogoFront = styled('span')(() => ({
+  '-moz-user-select': 'none',
+  '-webkit-user-select': 'none',
+  '-ms-user-select': 'none',
+  'user-select': 'none',
+  '-o-user-select': 'none',
 }));
 
 const Logo = () => {
   const theme = useTheme();
 
   return (
-    <Typography variant="h3">
-      One
+    <Typography
+      variant="h3"
+      style={{ cursor: 'pointer' }}
+    >
+      <LogoFront>
+        One
+      </LogoFront>
       <LogoHighlight theme={theme}>Thread</LogoHighlight>
     </Typography>
   );
