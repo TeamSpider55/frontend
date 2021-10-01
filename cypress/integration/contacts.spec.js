221f48027abea6d6f0d80566ea9edeecc6b5047e
jonjau <jonjau3q@gmail.com>
2021-10-01 11:56:42 +1000

    contact detail tests for editing and cancelling edits

--- cypress/integration/contacts.spec.js
+++ cypress/integration/contacts.spec.js
@@ -1,13 +1,13 @@
 describe('Contacts List', () => {
   beforeEach(() => {
     // load page with dummy contacts
-    cy.visit('/contacts?dummy=true')
-  })
+    cy.visit('/contacts?dummy=true');
+  });
 
   it('can be navigated to via the sidebar', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.get('ul').find('a').contains('Contacts').click();
-  })
+  });
 
   it('shows correct table headers', () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
@@ -18,98 +18,155 @@ describe('Contacts List', () => {
     cy.get('@tableHeaderRow').contains('Date Added');
   });
 
-  it('can sort by \'Name\' table header', () => {
+  it("can sort by 'Name' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Name').click();
 
     // arrow should be indicated
-    cy.get('@tableHeaderRow').contains('Name').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Name')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
 
     // expect the second item to be gte than the first after sorting
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
     let firstVal;
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         firstVal = normalizeText($p.text());
-    });
-    cy.get('table').find('tbody>tr').first().next().find('p').should(($p) => {
+      });
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .next()
+      .find('p')
+      .should(($p) => {
         const secondVal = normalizeText($p.text());
         assert.operator(secondVal, '>=', firstVal);
-    });
+      });
   });
 
-  it('can sort by \'Organisation\' table header', () => {
+  it("can sort by 'Organisation' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Organisation').click();
 
-    cy.get('@tableHeaderRow').contains('Organisation').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Organisation')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
 
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
     let firstVal;
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         firstVal = normalizeText($p.text());
-    });
-    cy.get('table').find('tbody>tr').first().next().find('p').should(($p) => {
+      });
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .next()
+      .find('p')
+      .should(($p) => {
         const secondVal = normalizeText($p.text());
         assert.operator(secondVal, '>=', firstVal);
-    });
+      });
   });
 
-  it('can sort by \'Role\' table header', () => {
+  it("can sort by 'Role' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Role').click();
 
-    cy.get('@tableHeaderRow').contains('Role').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Role')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
 
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
     let firstVal;
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         firstVal = normalizeText($p.text());
-    });
-    cy.get('table').find('tbody>tr').first().next().find('p').should(($p) => {
+      });
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .next()
+      .find('p')
+      .should(($p) => {
         const secondVal = normalizeText($p.text());
         assert.operator(secondVal, '>=', firstVal);
-    });
+      });
   });
 
-  it('can sort by \'Date Added\' table header', () => {
+  it("can sort by 'Date Added' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Date Added').click();
 
-    cy.get('@tableHeaderRow').contains('Date Added').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Date Added')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
 
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
     let firstVal;
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         firstVal = normalizeText($p.text());
-    });
-    cy.get('table').find('tbody>tr').first().next().find('p').should(($p) => {
+      });
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .next()
+      .find('p')
+      .should(($p) => {
         const secondVal = normalizeText($p.text());
         assert.operator(secondVal, '>=', firstVal);
-    });
+      });
   });
 
   it('can search by name', () => {
-    cy.get('[placeholder="Search..."]').should('match', 'input')
-      .type('given9');
+    cy.get('[placeholder="Search..."]').should('match', 'input').type('given9');
 
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         const val = normalizeText($p.text());
         if (!val.match(/\\*given9\\*/)) {
-            throw new Error(`Could not find the searched text`);
+          throw new Error(`Could not find the searched text`);
         }
-    });
+      });
   });
 
   it('can select multiple', () => {
-    cy.get('table').find('tbody>tr [type="checkbox"]').first()
-      .should('match', 'input').click();
+    cy.get('table')
+      .find('tbody>tr [type="checkbox"]')
+      .first()
+      .should('match', 'input')
+      .click();
 
-    cy.get('table').find('tbody>tr [type="checkbox"]').last()
-      .should('match', 'input').click();
+    cy.get('table')
+      .find('tbody>tr [type="checkbox"]')
+      .last()
+      .should('match', 'input')
+      .click();
   });
 });
--- cypress/integration/events.spec.js
+++ cypress/integration/events.spec.js
@@ -1,13 +1,13 @@
 describe('Events List', () => {
   beforeEach(() => {
     // load page with dummy events
-    cy.visit('/events?dummy=true')
-  })
+    cy.visit('/events?dummy=true');
+  });
 
   it('can be navigated to via the sidebar', () => {
-    cy.visit('/')
+    cy.visit('/');
     cy.get('ul').find('a').contains('Schedule').click();
-  })
+  });
 
   it('shows correct table headers', () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
@@ -17,41 +17,62 @@ describe('Events List', () => {
     cy.get('@tableHeaderRow').contains('Participants');
   });
 
-  it('can sort by \'Title\' table header', () => {
+  it("can sort by 'Title' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Title').click();
 
     // arrow should be indicated
-    cy.get('@tableHeaderRow').contains('Title').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Title')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
 
     // expect the second item to be gte than the first after sorting
-    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase()
+    const normalizeText = (s) => s.replace(/\s/g, '').toLowerCase();
     let firstVal;
-    cy.get('table').find('tbody>tr').first().find('p').then(($p) => {
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .find('p')
+      .then(($p) => {
         firstVal = normalizeText($p.text());
-    });
-    cy.get('table').find('tbody>tr').first().next().find('p').should(($p) => {
+      });
+    cy.get('table')
+      .find('tbody>tr')
+      .first()
+      .next()
+      .find('p')
+      .should(($p) => {
         const secondVal = normalizeText($p.text());
         assert.operator(secondVal, '>=', firstVal);
-    });
+      });
   });
 
-  it('can sort by \'Time\' table header', () => {
+  it("can sort by 'Time' table header", () => {
     cy.get('table').find('thead>tr').as('tableHeaderRow');
     cy.get('@tableHeaderRow').contains('Time').click();
 
     // arrow should be indicated
-    cy.get('@tableHeaderRow').contains('Time').find('svg').first()
+    cy.get('@tableHeaderRow')
+      .contains('Time')
+      .find('svg')
+      .first()
       .should('have.attr', 'data-testid', 'ArrowDownwardIcon');
   });
 
   it('can select multiple', () => {
-    cy.get('table').find('tbody>tr [type="checkbox"]').first()
-      .should('match', 'input').click();
+    cy.get('table')
+      .find('tbody>tr [type="checkbox"]')
+      .first()
+      .should('match', 'input')
+      .click();
 
-    cy.get('table').find('tbody>tr [type="checkbox"]').last()
-      .should('match', 'input').click();
+    cy.get('table')
+      .find('tbody>tr [type="checkbox"]')
+      .last()
+      .should('match', 'input')
+      .click();
   });
 
   it('can search by valid date by typing (leap year test)', () => {
