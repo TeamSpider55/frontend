5546e857f728f396542ecdfe7e89fa7d2c21132a
jonjau <jonjau3q@gmail.com>
2021-11-14 18:08:32 +1100

    fix api url

--- src/util/constants.ts
+++ src/util/constants.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:8080';
-// const API_URL = 'https://spider55-api.herokuapp.com';
+// const API_URL = 'http://localhost:8080';
+const API_URL = 'https://spider55-api.herokuapp.com';
 
 export default API_URL;
